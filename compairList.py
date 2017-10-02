f5b5be6b7ef36ed1e340aef6e324e66db8e6d3c8
tycoon87 <tyson.woodruff@outlook.com>
2017-10-02 16:39:41 -0700

    works

---
+++ compairList.py
@@ -0,0 +1,9 @@
+list_one = [1,2,5,6,2]
+list_two = [1,2,5,4,2]
+
+def compare(list1, list2):
+    if list1 == list2:
+        print "The lists are the same."
+    else:
+        print "the lists are not the same."
+print compare(list_one, list_two)
